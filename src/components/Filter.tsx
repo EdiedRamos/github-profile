c5b649d8c3cb0c1f1957328ada6d815888ef5fb8
EdiedRamos <edieisa22@gmail.com>
2024-05-09 22:18:27 -0500

    style: update filter styles

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,11 +1,20 @@
 import { SearchIcon } from "@/assets";
+import { useState } from "react";
 
 export const Filter = () => {
+  const [inputFocus, setInputFocus] = useState(false);
+
   return (
     <div className="pt-[32px]">
-      <div className="flex gap-2 bg-cc-midnight-blue mx-auto max-w-[400px] p-[16px] rounded-[16px]">
+      <div
+        className={`flex gap-2 bg-cc-midnight-blue mx-auto max-w-[400px] p-[16px] rounded-[16px] ${
+          inputFocus ? "border-2 border-cc-vibrant-blue" : ""
+        }`}
+      >
         <SearchIcon />
         <input
+          onFocus={() => setInputFocus(true)}
+          onBlur={() => setInputFocus(false)}
           className="w-full outline-none bg-cc-midnight-blue text-cc-misty-gray text-cs-200"
           type="text"
           placeholder="username"
