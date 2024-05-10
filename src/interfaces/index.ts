fc8a03a1a5a16f88f27b76d57a61846ee20bf700
EdiedRamos <edieisa22@gmail.com>
2024-05-09 21:49:23 -0500

    chore: add interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,21 @@
+interface License {
+  spdx_id: string;
+}
+
+export interface Profile {
+  name: string;
+  avatar_url: string;
+  bio: string;
+  followers: number;
+  following: number;
+  location: string;
+}
+
+export interface Repository {
+  name: string;
+  description: string;
+  forks_count: number;
+  updated_at: string;
+  license: License | null;
+  stargazers_count: number;
+}
