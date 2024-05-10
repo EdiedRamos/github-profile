779cb647d02ab0a6d11fac44d9632c70e8d96c43
EdiedRamos <edieisa22@gmail.com>
2024-05-09 21:13:28 -0500

    style: implement filter design

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,16 @@
+import { SearchIcon } from "@/assets";
+
 export const Filter = () => {
   return (
-    <div>
-      <input type="text" placeholder="username" />
+    <div className="pt-[32px]">
+      <div className="flex gap-2 bg-cc-midnight-blue mx-auto max-w-[400px] p-[16px] rounded-[16px]">
+        <SearchIcon />
+        <input
+          className="bg-cc-midnight-blue text-cc-misty-gray outline-none"
+          type="text"
+          placeholder="username"
+        />
+      </div>
     </div>
   );
 };
