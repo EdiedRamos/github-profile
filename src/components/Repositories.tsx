150afd783c0644d3bc6c3ce07e44ec19b2b5011d
EdiedRamos <edieisa22@gmail.com>
2024-05-10 17:30:52 -0500

    chore: implement grid in repositories container

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -5,13 +5,15 @@ export const Repositories = () => {
   const { repositories } = useGithub();
 
   return (
-    <section className="bg-cc-midnight-navy">
-      {repositories.map((repository) => (
-        <RepositoryInfo key={repository.id} repository={repository} />
-      ))}
-      <p className="text-center text-green-400">
-        View all repositories {repositories.length}
-      </p>
+    <section className="bg-cc-midnight-navy pt-8">
+      <div className="custom-container grid grid-cols-1 lg:grid-cols-2 gap-5">
+        {repositories.map((repository) => (
+          <RepositoryInfo key={repository.id} repository={repository} />
+        ))}
+        <p className="text-center text-green-400">
+          View all repositories {repositories.length}
+        </p>
+      </div>
     </section>
   );
 };
