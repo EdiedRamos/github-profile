09ba395d0a00685a20d30a5450758d211ccc93c9
EdiedRamos <edieisa22@gmail.com>
2024-05-10 02:03:39 -0500

    style: udpate filter outline style

--- src/assets/SearchIcon.tsx
+++ src/assets/SearchIcon.tsx
@@ -1,6 +1,7 @@
-export const SearchIcon = () => {
+export const SearchIcon = ({ className = "" }: { className: string }) => {
   return (
     <svg
+      className={className}
       width="24"
       height="24"
       viewBox="0 0 24 24"
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,30 +1,19 @@
 import { SearchIcon } from "@/assets";
 import { useGithub } from "@/hook/useGithub";
-import { useState } from "react";
 
 export const Filter = () => {
   const { handleSearch } = useGithub();
 
-  const [inputFocus, setInputFocus] = useState(false);
-
-  const handleFocus = () => setInputFocus(true);
-  const handleBlur = () => setInputFocus(false);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     handleSearch(event.target.value);
 
   return (
     <div className="pt-[32px]">
-      <div
-        className={`flex gap-2 bg-cc-midnight-blue mx-auto max-w-[400px] p-[16px] rounded-[16px] ${
-          inputFocus ? "border-2 border-cc-vibrant-blue" : ""
-        }`}
-      >
-        <SearchIcon />
+      <div className={"relative flex mx-auto max-w-[400px]"}>
+        <SearchIcon className="absolute top-[50%] left-3 translate-y-[-50%] text-red-400" />
         <input
-          onFocus={handleFocus}
-          onBlur={handleBlur}
           onChange={handleChange}
-          className="w-full outline-none bg-cc-midnight-blue text-cc-misty-gray text-cs-200"
+          className="w-full pl-11 pr-4 h-[50px] rounded-[16px] focus:outline-none focus:ring focus:ring-cc-vibrant-blue bg-cc-midnight-blue text-cc-misty-gray text-cs-200"
           type="text"
           placeholder="username"
         />
