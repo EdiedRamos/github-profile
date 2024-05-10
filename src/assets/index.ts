94185b279768e14c9d5c5c670bd4ca0b17bb15a0
EdiedRamos <edieisa22@gmail.com>
2024-05-09 19:52:06 -0500

    chore: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as ChieldIcon } from "./Chield_alt.svg";
+export { default as NestingIcon } from "./Nesting.svg";
+export { default as SearchIcon } from "./Search.svg";
+export { default as StarIcon } from "./Star.svg";
